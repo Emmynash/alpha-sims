0f0cd0058760243ee17277170b056a0e4e5a2626
uhweeka ika danjuma <uhweka@gmail.com>
2021-04-16 12:40:23 +0100

    school fee module started

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -12,24 +12,24 @@ class WebhookController extends Controller
     public function handle(Request $request)
     {
 
-        Log::info($request->server());
+        // Log::info($request->server());
 
 
         try {
-            if ((strtoupper($request->server()['REQUEST_METHOD']) != 'POST' ) || !array_key_exists('x-paystack-signature', $request->server()) ){
-                exit();
-            }
+            // if ((strtoupper($request->server()['REQUEST_METHOD']) != 'POST' ) || !array_key_exists('x-paystack-signature', $request->server()) ){
+            //     exit();
+            // }
     
             $paymentDetails = $request;
     
-            $sk_key = PaymentDetails::where("schoolid", $paymentDetails['data']['metadata']['schoolid'])->first();
+            // $sk_key = PaymentDetails::where("schoolid", $paymentDetails['data']['metadata']['schoolid'])->first();
     
-            $input = @file_get_contents("php://input");
-            define('PAYSTACK_SECRET_KEY', $sk_key->paystack_sk);
+            // $input = @file_get_contents("php://input");
+            // define('PAYSTACK_SECRET_KEY', $sk_key->paystack_sk);
     
-            if($request->server()['HTTP_X_PAYSTACK_SIGNATURE'] !== hash_hmac('sha512', $input, PAYSTACK_SECRET_KEY)) exit();
+            // if($request->server()['HTTP_X_PAYSTACK_SIGNATURE'] !== hash_hmac('sha512', $input, PAYSTACK_SECRET_KEY)) exit();
     
-            Log::debug($request);
+            // Log::debug($request);
     
             
     
