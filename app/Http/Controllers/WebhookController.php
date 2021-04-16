e8e21700b6888f45573c2ba04fdfc1c85ea5dad2
uhweeka ika danjuma <uhweka@gmail.com>
2021-04-16 09:57:38 +0100

    school fee module started

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\FeesInvoice;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -10,5 +11,31 @@ class WebhookController extends Controller
     public function handle(Request $request)
     {
         Log::debug($request);
+
+        $paymentDetails = $request;
+
+        $paymentcode = $paymentDetails['data']['metadata']['paymentcode'];
+
+        if ($paymentcode == "1") {
+            
+            $schoolid = $paymentDetails['data']['metadata']['schoolid'];
+
+            $systemno = $paymentDetails['data']['metadata']['systemno'];
+    
+            $schoolid = $paymentDetails['data']['metadata']['schoolid'];
+
+            $session = $paymentDetails['data']['metadata']['session'];
+    
+            $amount = $paymentDetails['data']['amount']/100;
+    
+            $ref = $paymentDetails['data']['reference'];
+
+
+            $updatePayment = FeesInvoice::where(['schoolid'=>$schoolid, 'session'=>$session, 'system_id'=>$systemno])->first();
+
+            $updatePayment->status = 1;
+            $updatePayment->save();
+
+        }
     }
 }
