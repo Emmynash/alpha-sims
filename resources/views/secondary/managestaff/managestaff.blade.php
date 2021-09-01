747fdf39fee2a1e35cc26c5f9d3ae0bbf77a3ec4
Uhweeka Ika Danjuma <uhweka@gmail.com>
2021-09-01 05:23:09 +0100

    test

--- resources/views/secondary/managestaff/managestaff.blade.php
+++ resources/views/secondary/managestaff/managestaff.blade.php
@@ -51,7 +51,7 @@
     </div>
   </footer>
 
-  <script src="{{ asset('js/appmanagestaff.js?v=1.1') }}"></script>
+  <script src="{{ asset('js/appmanagestaff.js?v=2') }}"></script>
 
   <script>
       function scrollocation(){
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PromotionController_sec;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
