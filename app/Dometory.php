7c3f9033c5a1f3a9f85bdbfa5d3b1d4b8a9f93e9
Uhweeka Ika Danjuma <uhweka@gmail.com>
2021-11-04 18:30:10 +0100

    migration files added

--- app/Dometory.php
+++ app/Dometory.php
@@ -2,9 +2,10 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Dometory extends Model
 {
-    //
+    use HasFactory;
 }
