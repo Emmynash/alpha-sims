6554b79dd610a69a7051cadc5f80c62ef3777e1e
Akita Emmanuel <e.akita91@gmail.com>
2022-04-04 08:01:55 +0100

    Update: component design

--- resources/js/result/generateresult.jsx
+++ resources/js/result/generateresult.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import {useEffect, useState} from 'react';
 import { useAlert } from 'react-alert'
+import moment from 'moment';
 
 function GenerateResult() {
 
@@ -22,7 +23,7 @@ function GenerateResult() {
             timeout: 2000, // custom timeout just for this one alert
             type: type,
             onOpen: () => {
-              console.log('hey')
+              console.log('open')
             }, // callback that will be executed after this alert open
             onClose: () => {
               console.log('closed')
@@ -33,7 +34,7 @@ function GenerateResult() {
     function getReadyResult() {
         setisLoading(true)
         axios.get('/sec/result/get_result_ready_section').then(response=>{
-            console.log(response)
+            // console.log(response)
             setisLoading(false)
             setgetReadyResults(response.data.getReadyResults)
         }).catch(e=>{
@@ -54,10 +55,10 @@ function GenerateResult() {
             }
         }).then(response=>{
             console.log(response)
+            window.location.reload();
             setisLoading(false)
-            getReadyResult()
 
-            if(response.status == 200){
+            if (response.status == 200) {
                 myalert('Success', 'success')
             }else{
                 myalert('failed', 'error')
@@ -84,21 +85,32 @@ function GenerateResult() {
             <div className="card">
                 <div style={{ margin:'10px' }}>
                     <div className="alert alert-info">
-                        <i>Result Generation is done per section. Below are list of available results for final processing if there is none means none is ready.</i>
+                        <i>Results can be generated multiple times per section by clicking on the Generate/Regenerate button.<br></br>
+                            See lists of available results for final processing below.
+                        </i>
                     </div>
                     <div>
-                        <p>List of available results for further Processing</p>
+                        <span style={{ fontSize: '14px', color: 'red' }}>
+                            Note: if the list is empty, it means no result has been submitted by form masters/mistresses for processing.
+                        </span>
                     </div>
                     <div>
                         {getReadyResults.map(d=>(
                             <div key={d.id+"resultready"} className="card" style={{ display:'flex', flexDirection:'row', alignItems:'center'}}>
                                 <div style={{ display:'flex', flexDirection:'column', marginLeft:'10px', flex:'1' }}>
-                                    <i style={{ fontStyle:'normal', fontSize:'13px' }}>Class: {d.classname}{d.sectionname}</i>
-                                    <i style={{ fontStyle:'normal', fontSize:'13px' }}>Status: {d.status == 0 ? "Pending":"Completed"}</i>
-                                    <i style={{ fontStyle:'normal', fontSize:'13px' }}>Date Generated: {d.status != 0 ? d.updated_at:"Not Generated"}</i>
+                                    <i style={{ fontStyle: 'normal', fontSize: '13px' }}>
+                                        <span style={{ color: "black", fontWeight: 'bold' }}>Class</span>: {d.classname}{d.sectionname}
+                                    </i>
+                                    <i style={{ fontStyle: 'normal', fontSize: '13px' }}>
+                                        <span style={{ color: "black", fontWeight: 'bold' }}>Status</span>: {d.status == 0 ? <span style={{ color: "yellow", fontWeight: 'bold' }}>Pending</span> :
+                                                <span style={{ color: "green", fontWeight: 'bold' }}>Completed</span>}</i>
+                                    <i style={{ fontStyle: 'normal', fontSize: '13px' }}>
+                                        <span style={{ color: "black", fontWeight: 'bold' }}>Generated</span>  :
+                                        {d.status != 0 ? moment(d.updated_at).fromNow() : <span style={{ color: "red", fontWeight: 'bold' }}>Not Generated</span>}
+                                    </i>
                                 </div>
                                 <div className="form-group" style={{ marginRight:'10px' }}>
-                                    <button onClick={()=>generateResultMain(d.classid, d.sectionid, d.id)} className= {d.status != 0 ? "btn btn-sm btn-warning badge":"btn btn-sm btn-success badge"}> {d.status != 0 ? "Generated":"Generate"}</button>
+                                    <button onClick={()=>generateResultMain(d.classid, d.sectionid, d.id)} className= {d.status != 0 ? "btn btn-sm btn-warning badge":"btn btn-sm btn-success badge"}> {d.status != 0 ? "Regenerate":"Generate"}</button>
                                 </div>
                             </div>
                         )) }
